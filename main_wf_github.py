274f4181158e995216d4aa7e22d2c9819ae2c54b
Redhoxx <red.h0x50@gmail.com>
2025-01-12 00:50:57 +0100

    test ytb video prédiction github

--- main_wf_github.py
+++ main_wf_github.py
@@ -1,5 +1,5 @@
 import sys
-
+from pytube import YouTube
 from src.audio_predictor import AudioPredictor
 
 audio_predictor = AudioPredictor(
@@ -7,9 +7,26 @@ audio_predictor = AudioPredictor(
                  model_dir="data/models",
                  raw_audio_dir="data/raw_to_predict")
 
+def download_audio_from_youtube(video_url):
+    try:
+        yt = YouTube(video_url)
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        audio_file = audio_stream.download(filename="youtube_audio.mp3")
+        return audio_file
+    except Exception as e:
+        print(f"Erreur lors du téléchargement de l'audio YouTube: {e}")
+        return None
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        audio_file = sys.argv[1]
-        predicted_genre = audio_predictor.predict_long_first_audio_in_dir(audio_file)
+        video_url = sys.argv[1]
+
+        audio_file = download_audio_from_youtube(video_url)
+
+        if audio_file:
+            predicted_genre = audio_predictor.predict_long_audio(audio_file)
+            print(f"Genre prédit: {predicted_genre}")
+        else:
+            print("Erreur: Impossible de télécharger l'audio de la vidéo YouTube.")
     else:
-        print("Erreur : Veuillez fournir le chemin du fichier audio en argument.")
+        print("Erreur: Veuillez fournir l'URL de la vidéo YouTube en argument.")
